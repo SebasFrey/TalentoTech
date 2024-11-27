0eddbc0cbc44d7028632be07b3bc6df51afa1ab9
SebasFrey <lomasshidokis@gmail.com>
2024-11-26 20:24:12 -0500

    Delete Talento Tech directory

--- Talento Tech/Codigos JS/ReciboDeLuz.js
+++
@@ -1,8 +0,0 @@
-let ca = parseFloat(prompt("Ingrese Consumo Actual: "));
-let cp = parseFloat(prompt("Ingrese Consumo Anterior: "));
-let vk = parseFloat(prompt("Ingrese Valor kw: "));
-
-let consumo = ca - cp;
-let total = consumo * vk;
-
-console.log("El costo total es: " + total);
